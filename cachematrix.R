d640741d5a64b95964248c9c8f4ef4666c3688f4
岩井聡 <stihahi@iwaisatshinoair.wifilpr.local.net>
2015-07-14 22:12:00 +1000

    cache matrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+##makeCacheMatrix : makes special matrix that can cache inverse matrix
+##To be easily understood, "im" is used for an attribute.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## make special matrix.
+makeCacheMatrix <- function(x = matrix()){
+    im <- NULL
+    set <- function(y){
+        x<<-y
+        im<<-NULL
+    }
+    get <- function() x
+    setIMatrix <- function(mat) im<<-mat
+    getIMatrix <- function() im
+    list(set = set,get = get,
+         setIMatrix = setIMatrix,
+         getIMatrix = getIMatrix)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##caculate inverse matrix as necessary, and caches it.
+##when there is already cache, print message that the program is using the cache.
+cacheSolve <- function(x, ...){
+    im<-x$getIMatrix()
+    if(!is.null(im)){
+        message("getting cache data")
+        return(im)
+    }
+    data <- x$get()
+    im <- solve(data, ...)
+    x$setIMatrix(im)
+    im
 }
